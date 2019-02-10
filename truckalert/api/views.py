e795a8ae13d7a8966743806886de70e4d534dd5a
root <root@studvm602.idi.ntnu.no>
2019-02-10 19:08:00 +0100

    Fix API urls

--- truckalert/api/views.py
+++ truckalert/api/views.py
@@ -9,14 +9,14 @@ from api.models import Position
 
 class UpdatePosition(APIView):
     
-    def get_pos_object(uid):
+    def get_pos_object(self, uid):
         try:
             return Position.objects.get(user_id=uid)
         except:
             return False
     
     
-    def app_key_valid(key):
+    def app_key_valid(self, key):
         #implement application key validation
         return True
 
@@ -24,12 +24,12 @@ class UpdatePosition(APIView):
 
         app_key = request.data['app_key']
 
-        if not app_key_valid(app_key): #check validity of the application key
+        if not self.app_key_valid(app_key): #check validity of the application key
             return Response(serializer.errors, status=status.HTTP_401_UNAUTHORIZED)
 
         uid = request.data['data']['user_id']
-        pos_object = get_pos_object(uid)
-        serializer = PositionSerializer(request.data['data'])
+        pos_object = self.get_pos_object(uid)
+        serializer = PositionSerializer(data=request.data['data'])
 
         if not serializer.is_valid():
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
